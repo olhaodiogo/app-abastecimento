a1f530ef013c0853b20426325b5909e940579837
olhaodiogo <diogosouzatst@gmail.com>
2026-03-17 15:04:44 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "app-v1";
+const urls = ["index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(urls)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
